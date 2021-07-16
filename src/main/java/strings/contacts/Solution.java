837911cb4dd26db2e3ca8057687e712e2bce8677
Clairton Carneiro Luz <clairton.luz@seduc.ce.gov.br>
2021-07-16 19:23:49 -0300

    implementing search by contact with better performance

--- src/main/java/strings/contacts/Solution.java
+++ src/main/java/strings/contacts/Solution.java
@@ -2,13 +2,14 @@ package strings.contacts;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Solution {
 
     public static List<Integer> contacts(List<List<String>> queries) {
-        var result = new ArrayList<Integer>();
-        var database = new HashMap<String, List<String>>();
+        var result = new LinkedList<Integer>();
+        var database = new HashMap<String, Integer>();
 
         for (var operation : queries) {
             var command = operation.get(0);
@@ -17,18 +18,15 @@ public class Solution {
                 for (int i = 1; i <= value.length(); i++) {
                     var subValue = value.substring(0, i);
                     if (!database.containsKey(subValue)) {
-                        var values = new ArrayList<String>();
-                        values.add(value);
-                        database.put(subValue, values);
+                        database.put(subValue, 1);
                     } else {
-                        database.get(subValue).add(value);
+                        database.put(subValue, database.get(subValue) + 1);
                     }
                 }
             }
 
             if (command.equals("find")) {
-                int count = database.getOrDefault(value, new ArrayList<>()).size();
-                result.add(count);
+                result.add(database.getOrDefault(value, 0));
             }
         }
 
