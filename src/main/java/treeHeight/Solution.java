419c31d715bdb3256a6e5f1006d77909e2a1ab66
Clairton Carneiro Luz <clairton.luz@seduc.ce.gov.br>
2021-07-16 16:40:22 -0300

    tree height

---
+++ src/main/java/treeHeight/Solution.java
@@ -0,0 +1,27 @@
+//package treeHeight;
+//
+//import org.w3c.dom.Node;
+//
+//import java.util.Scanner;
+//
+//public class Solution {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//
+//        int numberOfNodes = scan.nextInt();
+//
+//        Node root = null;
+//        for (int i = 0; i < numberOfNodes; i++) {
+//            int data = scan.nextInt();
+//
+//            if (root == null) {
+//                root = new Node(data);
+//            } else {
+//                root.add(new Node(data));
+//            }
+//        }
+//        scan.close();
+//
+//        System.out.println(root == null ? -1 : root.getHeight());
+//    }
+//}
